8b65ca0e4da1f876554b29780cd2caac28a4fe23
Davishiio <dtdavid@gmail.com>
2026-01-13 05:54:57 -0500

    Se agregó lógica en onMounted para recuperar la sesión del usuario ( fetchUser ) si existe un token almacenado. Esto soluciona la pérdida de datos del usuario al recargar la página.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { onMounted } from 'vue';
+import { RouterView } from 'vue-router';
+import { useAuthStore } from './stores/auth';
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  if (authStore.token) {
+    authStore.fetchUser();
+  }
+});
 </script>
 
 <template>
